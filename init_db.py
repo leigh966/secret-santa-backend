293a71bfa1a0b02d2d12449c8812576522e5d9d2
leigh966 <leigh966@hotmail.com>
2022-08-31 17:34:58 +0100

    Created db_config to hold constants about the database

--- init_db.py
+++ init_db.py
@@ -1,9 +1,10 @@
 import sqlite3
+from db_config import DATABASE_FILENAME, INIT_SCHEMA_FILENAME
 
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect(DATABASE_FILENAME)
 
 
-with open('schema.sql') as f:
+with open(INIT_SCHEMA_FILENAME) as f:
     connection.executescript(f.read())
 
 
