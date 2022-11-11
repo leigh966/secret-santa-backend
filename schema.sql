16502dad38a24f6e6c870bc5936ec0b1a2ca2e66
leigh966 <leigh966@hotmail.com>
2022-11-11 12:25:01 +0000

    Modified database to include groups, group functionality not yet implemented on endpoints, registration may be broken until updated to handle groups

--- schema.sql
+++ schema.sql
@@ -1,6 +1,12 @@
 DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS players;
 DROP TABLE IF EXISTS games_and_players;
+DROP TABLE IF EXISTS groups;
+
+CREATE TABLE groups (
+    group_id INTEGER PRIMARY KEY,
+    group_name VARCHAR(20) NOT NULL
+);
 
 CREATE TABLE games (
     game_id INTEGER PRIMARY KEY,
@@ -12,7 +18,9 @@ CREATE TABLE players (
     player_id INTEGER PRIMARY KEY,
     password_hash VARCHAR(100) NOT NULL,
     player_name VARCHAR(20) NOT NULL,
-    picked_name VARCHAR(20) DEFAULT NULL
+    picked_name VARCHAR(20) DEFAULT NULL,
+    group_id INTEGER,
+    FOREIGN KEY(group_id) REFERENCES groups(group_id)
 );
 
 CREATE TABLE games_and_players (
