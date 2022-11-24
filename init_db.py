7d7530c95fe5fc1159a0be3d4cbdc57fd704454c
leigh966 <leigh966@hotmail.com>
2022-11-24 15:35:30 +0000

    Removed incomplete and unused line 'cursor.exec'

--- init_db.py
+++ init_db.py
@@ -3,7 +3,6 @@ from db_config import INIT_SCHEMA_FILENAME
 
 connection = getConnection()
 cursor = connection.cursor()
-cursor.exec
 
 with open(INIT_SCHEMA_FILENAME) as f:
     cursor.execute(f.read())
